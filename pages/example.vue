6ee566cc83bdc3e0b9f4deaad797c50bd0afedfc
Fuxing Loh <git@fuxing.dev>
2020-10-07 12:39:49 +0800

    Complete 5 examples

---
+++ pages/example.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <example-header/>
+
+    <example-horizontal class="padding-top"/>
+
+    <example-full-width-card class="padding-top"/>
+
+    <example-side-by-side class="padding-top"/>
+
+    <example-waterfall class="padding-top"/>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  padding-bottom: 256px;
+}
+
+.padding-top {
+  padding-top: 64px;
+}
+</style>
