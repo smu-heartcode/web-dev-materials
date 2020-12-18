4af006cb3c8950d0e02fd37b6f8278eec7de80e5
Fuxing Loh <git@fuxing.dev>
2020-12-18 19:27:10 +0800

    Removed components library

--- @smu-heartcode/components-lib/src/lib-components/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as HeartcodeBetween } from './heartcode-between.vue';
-export { default as HeartcodeContainer } from './heartcode-container.vue';
-export { default as HeartcodeHeader } from './heartcode-header.vue';
-export { default as HeartcodeImage } from './heartcode-image.vue';
-export { default as HeartcodeList } from './heartcode-list.vue';
--- @smu-heartcode/components/plugin.js
+++ @smu-heartcode/components/plugin.js
@@ -6,17 +6,3 @@ import VueHorizontal from 'vue-horizontal'
 Vue.use(VueHorizontal)
 Vue.use(VueHorizontalList)
 Vue.use(VueMasonryWall)
-
-import {
-  HeartcodeBetween,
-  HeartcodeContainer,
-  HeartcodeHeader,
-  HeartcodeImage,
-  HeartcodeList
-} from '@smu-heartcode/smu-heartcode-components-lib'
-
-Vue.component('heartcode-between', HeartcodeBetween)
-Vue.component('heartcode-header', HeartcodeHeader)
-Vue.component('heartcode-container', HeartcodeContainer)
-Vue.component('heartcode-image', HeartcodeImage)
-Vue.component('heartcode-list', HeartcodeList)
